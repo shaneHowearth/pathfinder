612022f9740eb03286f88a928073c7a57a75f9b9
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-08-10 16:23:44 +1000

    Start BFS package

---
+++ bfs/bfs.go
@@ -0,0 +1,7 @@
+// Package bfs - Breadth First Search
+// Starting at the root node this algorithm searches each edge that leads to a
+// child node, in search of the goal node. Once all the edges leading out of the
+// node have been searched, the algorithm moves to search each of the next level
+// down (the children nodes found in the previous search) in the same way,
+// continuing until the goal is found, or there are no more nodes to search.
+package bfs
