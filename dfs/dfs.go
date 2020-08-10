011a017b15b204033868b931f44282161b5def29
Shane Howearth <Shane.Howearth@shanehowearth.com>
2020-08-10 16:34:58 +1000

    Start Depth First Search package

---
+++ dfs/dfs.go
@@ -0,0 +1,7 @@
+// Package dfs - Depth First Search
+// Starting at the root node this algorithm searches the right most edge leading
+// out of the node, that hasn't yet been searched, until it reachs the bottom of
+// the search tree, or the goal. It then goes back up one node and searches the
+// right most edge, and so on until the tree has been completely searched, or
+// the goal has been found.
+package dfs
